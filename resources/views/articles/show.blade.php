103a8b6d65d93804f5c4af3dd1a48d37fdf6c5be
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-02-26 16:07:41 +0100

    Creata una condizione per stampare In show i tags se relazionati
    Creata una condizione per stampare In show i tags se relazionati

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -7,6 +7,15 @@
             <p class="data"><strong>CREATED: </strong>{{$article->created_at}}</p>
             <p class="data"><strong>UPDATED: </strong>{{$article->updated_at}}</p>
             <span><strong>CATEGORY: </strong>{{$article->category->name}}</span>
+            <div><strong>TAGS: </strong>
+            @if( count($article->tags) > 0 )
+                @foreach ($article->tags as $tag)
+                    <span>{{$tag->tag}}</span>
+                @endforeach
+            @else
+                <span>N/A</span>
+            @endif
+            </div>
         </div>
         <a href="{{ route('blog') }}" class="btn">RETURN</a>
     </div>
