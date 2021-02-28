20d01fc24e30348cd8d97d8f5da23717742a98ed
marcocaffarelli <caffarelli-m94@hotmail.it>
2021-03-01 00:51:56 +0100

    Aggiunta la category allo show dell' article
    Aggiunta la category allo show dell' article

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -6,7 +6,7 @@
             <p class="body"><strong>BODY: </strong>{{$article->description}}</p>
             <p class="data"><strong>CREATED: </strong>{{$article->created_at}}</p>
             <p class="data"><strong>UPDATED: </strong>{{$article->updated_at}}</p>
-
+            <span><strong>CATEGORY: {{$article->category ? $article->category->name : 'N/A'}}</strong></span><br>
             <!-- tags -->
             <span><strong>TAGS: </strong>
             @if( count($article->tags) > 0 )
